801bb287e5f8d537b232149b940ac36cca068dcc
tamer <tamerna@post.bgu.ac.il>
2021-10-02 01:48:05 +0300

    Initialization

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import Network.TCPHandler;
+
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello world");
+        TCPHandler tcpHandler = new TCPHandler(443) {
+            @Override
+            public void onConnectionRequest(Socket socket) {
+                System.out.println("Connection");
+            }
+        };
+
+        tcpHandler.start();
     }
 
 }
