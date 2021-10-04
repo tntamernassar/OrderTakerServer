de42c4f8de3155b4ecbbbc9afce53cf5987e01c8
tamer <tamerna@post.bgu.ac.il>
2021-10-04 17:43:03 +0300

    Initialization

---
+++ src/TestClient.java
@@ -0,0 +1,16 @@
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class TestClient {
+    public static void main(String[] args){
+        try{
+            Socket s = new Socket("193.123.85.54",80);
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("Hello Server");
+            dout.flush();
+            dout.close();
+            s.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+
+}
